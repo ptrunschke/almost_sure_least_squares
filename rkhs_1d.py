831bc0b4cb48f66b274d153b3340500c07e0e889
Philipp Trunschke <philipp.trunschke@univ-nantes.fr>
2024-05-02 15:59:16 +0200

    automate figure generation

--- rkhs_1d.py
+++ rkhs_1d.py
@@ -36,57 +36,12 @@ def kernel_matrix(kernel, xs):
 
 
 if __name__ == "__main__":
+    from pathlib import Path
     from basis_1d import compute_inner, orthonormalise
-    # space = "h1"
-    # space = "h10"
-    space = "h1gauss"
-
-    if space == "h10":
-        domain = (-1, 1)
-        reference_kernel = h10_kernel
-    elif space == "h1":
-        domain = (-1, 1)
-        reference_kernel = h1_kernel
-    elif space == "h1gauss":
-        domain = (-5, 5)
-        reference_kernel = h1gauss_kernel
-    else:
-        raise NotImplementedError()
-
-    tab20 = mpl.colormaps["tab20"].colors
-
-    # offset = 3
-    # js = np.arange(nx)[offset:-offset].reshape(5, -1)[:, 0]
-    # js = np.concatenate([js, [nx - offset]])
-    # for e, j in enumerate(js):
-    #     c = xs[j]
-    #     ks = K[:, j]
-    #     ks_ref = reference_kernel(xs, c)
-    #     plt.plot(xs, L[:, j], color=tab20[2 * e])
-    #     plt.plot(xs, ks, color=tab20[2 * e])
-    #     plt.plot(xs, ks_ref, color=tab20[2 * e + 1], linestyle=":")
-    # if space == "h1gauss":
-    #     plt.yscale("log")
-    # plt.show()
-
-    # I, xs = compute_inner(space, domain, 2 ** 12)
-    # L = sps.eye(I.shape[0], format="csc", dtype=float)
-    # I = I.tocsc()
-    # K = sps.linalg.spsolve(I, L).toarray()
-    # L = L.toarray()
-    # us = np.diag(K).copy()
-    # us_ref = reference_kernel(xs, xs)
-    # if space == "h10":
-    #     us[[0, -1]] = 0
-    # plt.plot(xs, us, color=tab20[0])
-    # plt.plot(xs, us_ref, color=tab20[1], linestyle=":")
-    # if space == "h1gauss":
-    #     plt.yscale("log")
-    # plt.show()
 
     dimension = 5
-    # basis = "polynomial"
-    basis = "fourier"
+    basis = "polynomial"
+    # basis = "fourier"
 
     def monomval(x, c, tensor=True):
         assert tensor
@@ -99,85 +54,141 @@ if __name__ == "__main__":
         values = c.T @ measures
         return values.reshape(*c_shape, *x_shape)
 
-    if space == "h10":
-        if basis == "polynomial":
-            def basisval(x):
-                values = monomval(x, np.eye(dimension))
-                return values * (x - 1) * (x + 1)
-        elif basis == "fourier":
-            def basisval(x):
-                x = (x - domain[0]) / (domain[1] - domain[0])
-                return np.sin(np.pi * x[None] * np.arange(1, dimension + 1)[:, None])
-        else:
-            raise NotImplementedError()
-    else:
-        if basis == "polynomial":
-            def basisval(x):
-                return monomval(x, np.eye(dimension))
-        elif basis == "fourier":
-            def basisval(x):
-                x = (x - domain[0]) / (domain[1] - domain[0])
-                c = dimension // 2 + (dimension % 2)
-                s = dimension // 2
-                assert c + s == dimension
-                z = np.ones((1, len(x)))
-                c = np.cos(2 * np.pi * x[None] * np.arange(1, c)[:, None])
-                s = np.sin(2 * np.pi * x[None] * np.arange(1, s + 1)[:, None])
-                assert z.shape[0] + c.shape[0] + s.shape[0] == dimension
-                return np.concatenate([z, c, s], axis=0)
+    for space in ["h10", "h1", "h1gauss"]:
+        print(f"Computing marginal densities for {space} with {basis} basis")
+        if space == "h10":
+            domain = (-1, 1)
+            reference_kernel = h10_kernel
+        elif space == "h1":
+            domain = (-1, 1)
+            reference_kernel = h1_kernel
+        elif space == "h1gauss":
+            domain = (-5, 5)
+            reference_kernel = h1gauss_kernel
         else:
             raise NotImplementedError()
 
-    # xs = np.linspace(*domain, 1000)
-    # for e, b in enumerate(basisval(xs)):
-    #     plt.plot(xs, b, label=f"{e}")
-    # plt.legend()
-    # plt.show()
+        tab20 = mpl.colormaps["tab20"].colors
 
-    if space == "h1gauss":
-        l2_basis = orthonormalise(basisval, "l2gauss", domain, 2 ** 12)
-    else:
-        l2_basis = orthonormalise(basisval, "l2", domain, 2 ** 12)
+        # offset = 3
+        # js = np.arange(nx)[offset:-offset].reshape(5, -1)[:, 0]
+        # js = np.concatenate([js, [nx - offset]])
+        # for e, j in enumerate(js):
+        #     c = xs[j]
+        #     ks = K[:, j]
+        #     ks_ref = reference_kernel(xs, c)
+        #     plt.plot(xs, L[:, j], color=tab20[2 * e])
+        #     plt.plot(xs, ks, color=tab20[2 * e])
+        #     plt.plot(xs, ks_ref, color=tab20[2 * e + 1], linestyle=":")
+        # if space == "h1gauss":
+        #     plt.yscale("log")
+        # plt.show()
 
-    rkhs_basis = orthonormalise(basisval, space, domain, 2 ** 12)
+        # I, xs = compute_inner(space, domain, 2 ** 12)
+        # L = sps.eye(I.shape[0], format="csc", dtype=float)
+        # I = I.tocsc()
+        # K = sps.linalg.spsolve(I, L).toarray()
+        # L = L.toarray()
+        # us = np.diag(K).copy()
+        # us_ref = reference_kernel(xs, xs)
+        # if space == "h10":
+        #     us[[0, -1]] = 0
+        # plt.plot(xs, us, color=tab20[0])
+        # plt.plot(xs, us_ref, color=tab20[1], linestyle=":")
+        # if space == "h1gauss":
+        #     plt.yscale("log")
+        # plt.show()
 
-    xs = np.linspace(*domain, 1002)[1:-1]  # remove the boundary points for the H10 case
-    M_onb = l2_basis(xs)
-    I_onb = rkhs_basis(xs)
+        if space == "h10":
+            if basis == "polynomial":
+                def basisval(x):
+                    values = monomval(x, np.eye(dimension))
+                    return values * (x - 1) * (x + 1)
+            elif basis == "fourier":
+                def basisval(x):
+                    x = (x - domain[0]) / (domain[1] - domain[0])
+                    return np.sin(np.pi * x[None] * np.arange(1, dimension + 1)[:, None])
+            else:
+                raise NotImplementedError()
+        else:
+            if basis == "polynomial":
+                def basisval(x):
+                    return monomval(x, np.eye(dimension))
+            elif basis == "fourier":
+                def basisval(x):
+                    x = (x - domain[0]) / (domain[1] - domain[0])
+                    c = dimension // 2 + (dimension % 2)
+                    s = dimension // 2
+                    assert c + s == dimension
+                    z = np.ones((1, len(x)))
+                    c = np.cos(2 * np.pi * x[None] * np.arange(1, c)[:, None])
+                    s = np.sin(2 * np.pi * x[None] * np.arange(1, s + 1)[:, None])
+                    assert z.shape[0] + c.shape[0] + s.shape[0] == dimension
+                    return np.concatenate([z, c, s], axis=0)
+            else:
+                raise NotImplementedError()
 
-    # for e, b in enumerate(M_onb):
-    #     plt.plot(xs, b, label=f"{e}")
-    # plt.legend()
-    # plt.show()
+        # xs = np.linspace(*domain, 1000)
+        # for e, b in enumerate(basisval(xs)):
+        #     plt.plot(xs, b, label=f"{e}")
+        # plt.legend()
+        # plt.show()
 
-    ch = np.sum(M_onb ** 2, axis=0)
-    if space == "h10":
-        ch *= 0.5
-    elif space == "h1":
-        ch *= 0.5
-    elif space == "h1gauss":
-        def rho(x):
-            return np.exp(-x**2 / 2) / np.sqrt(2 * np.pi)
+        if space == "h1gauss":
+            l2_basis = orthonormalise(basisval, "l2gauss", domain, 2 ** 12)
+        else:
+            l2_basis = orthonormalise(basisval, "l2", domain, 2 ** 12)
 
-        ch *= rho(xs)
+        rkhs_basis = orthonormalise(basisval, space, domain, 2 ** 12)
 
-    normalise = lambda ys: ys / np.trapz(ys, xs)
+        xs = np.linspace(*domain, 1002)[1:-1]  # remove the boundary points for the H10 case
+        M_onb = l2_basis(xs)
+        I_onb = rkhs_basis(xs)
 
-    kd = np.sum(I_onb ** 2, axis=0)
-    k = reference_kernel(xs, xs)
-    ratio = np.nan_to_num(kd / k)
-    plt.plot(xs, normalise(kd), color=tab20[0], label=r"$k_d(x,x)$")
-    plt.plot(xs, normalise(k), color=tab20[1], label=r"$k(x,x)$")
-    plt.plot(xs, normalise(ratio), color="tab:red", label=r"$\frac{k_d(x,x)}{k(x,x)}$")
-    # plt.plot(xs, normalise(ratio * rho(xs)), color="tab:purple", label=r"$\frac{k_d(x,x)}{k(x,x)} \rho$")
-    plt.plot(xs, normalise(ch), color="k", linestyle="--", label="Christoffel density")
-    plt.legend()
-    if space == "h10":
-        plt.title(r"Polynomial basis for $H^1_0(-1, 1)$")
-    elif space == "h1":
-        plt.ylim(0.25, 0.8)
-        plt.title(r"Polynomial basis for $H^1(-1, 1)$")
-    elif space == "h1gauss":
-        plt.ylim(-0.025, 0.25)
-        plt.title(r"Polynomial basis for $H^1_w(\mathbb{R})$ with $w(x) = \frac{1}{\sqrt{2\pi}}\exp(-\frac{x^2}{2})$")
-    plt.show()
+        # for e, b in enumerate(M_onb):
+        #     plt.plot(xs, b, label=f"{e}")
+        # plt.legend()
+        # plt.show()
+
+        ch = np.sum(M_onb ** 2, axis=0)
+        if space == "h10":
+            ch *= 0.5
+        elif space == "h1":
+            ch *= 0.5
+        elif space == "h1gauss":
+            def rho(x):
+                return np.exp(-x**2 / 2) / np.sqrt(2 * np.pi)
+
+            ch *= rho(xs)
+
+        normalise = lambda ys: ys / np.trapz(ys, xs)
+
+        kd = np.sum(I_onb ** 2, axis=0)
+        k = reference_kernel(xs, xs)
+        ratio = np.nan_to_num(kd / k)
+
+        fig, ax = plt.subplots(1, 1, figsize=(8, 4), dpi=300)
+        ax.plot(xs, normalise(kd), color=tab20[0], label=r"$k_d(x,x)$")
+        ax.plot(xs, normalise(k), color=tab20[1], label=r"$k(x,x)$")
+        ax.plot(xs, normalise(ratio), color="tab:red", label=r"$\frac{k_d(x,x)}{k(x,x)}$")
+        # plt.plot(xs, normalise(ratio * rho(xs)), color="tab:purple", label=r"$\frac{k_d(x,x)}{k(x,x)} \rho$")
+        ax.plot(xs, normalise(ch), color="k", linestyle="--", label="Christoffel density")
+        ax.legend()
+        if space == "h10":
+            # plt.title(r"Polynomial basis for $H^1_0(-1, 1)$")
+            pass
+        elif space == "h1":
+            ax.set_ylim(0.25, 0.8)
+            # plt.title(r"Polynomial basis for $H^1(-1, 1)$")
+        elif space == "h1gauss":
+            ax.set_ylim(-0.025, 0.25)
+            # plt.title(r"Polynomial basis for $H^1_w(\mathbb{R})$ with $w(x) = \frac{1}{\sqrt{2\pi}}\exp(-\frac{x^2}{2})$")
+
+        plot_directory = Path(__file__).parent / "plot"
+        plot_directory.mkdir(exist_ok=True)
+        plot_path = plot_directory / f"marginal_{space}_{basis}.png"
+        print("Saving sample statistics plot to", plot_path)
+        plt.savefig(
+            plot_path, dpi=300, edgecolor="none", bbox_inches="tight", transparent=True
+        )
+        plt.close(fig)
