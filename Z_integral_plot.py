28a6ade35a5e9d180de5c387e855e6e9a57f1ccc
Philipp Trunschke <philipp.trunschke@univ-nantes.fr>
2025-09-05 11:37:43 +0200

    feat: plot Z integrals for all RKHS

--- Z_integral_plot.py
+++ Z_integral_plot.py
@@ -6,11 +6,12 @@ import matplotlib.pyplot as plt
 
 from plotting import plotting
 
+data_directory = Path(__file__).parent / "plot"
+plot_directory = Path(__file__).parent / "plot"
+plot_directory.mkdir(parents=True, exist_ok=True)
+
 mpl.rc('text', usetex=True)
 mpl.rc('text.latex', preamble=r'\usepackage{amsmath,amssymb}')
-
-integrals = np.load("plot/integrals_h10_embedding_sampling.npy")
-dimension = len(integrals)
 
 geometry = {
     "top": 1,
@@ -20,26 +21,41 @@ geometry = {
     "wspace": 0.25,  # the default as defined in rcParams
     "hspace": 0.25,  # the default as defined in rcParams
 }
-figshape = (1, 1)
+figshape = (1, 3)
 textwidth = 6.50127  # width of the text in inches
 figwidth = textwidth  # width of the figure in inches
-# figwidth = textwidth / 2  # width of the figure in inches
 phi = (1 + np.sqrt(5)) / 2
-aspect_ratio = phi
+aspect_ratio = 1 / phi
 figsize = plotting.compute_figsize(geometry, figshape, aspect_ratio=aspect_ratio, figwidth=figwidth)
-# figsize = (figsize[0], figsize[1] * 1.5)
+# figsize = (1.15 * figsize[0], 1.15 * figsize[1] * 1.5)
 
-fig, ax = plt.subplots(*figshape, figsize=figsize, dpi=300)
-ax.violinplot(list(integrals), positions=np.arange(len(integrals)))
-ax.set_xlabel("$i$")
-ax.set_ylabel("$Z_i$", rotation=0)
-ax.set_yticks(np.arange(1, dimension+1))
-ax.yaxis.grid()
+fig, ax = plt.subplots(*figshape, figsize=figsize, sharey=True, dpi=300)
+ax = ax.reshape(-1)
+
+titles = [
+    r"$H^1([-1,1], \tfrac{1}{2}\mathrm{d}x)$",
+    r"$H^1_0([-1,1], \tfrac{1}{2}\mathrm{d}x)$",
+    r"$H^1(\mathbb{R}, \mathcal{N}(0, 1))$",
+]
+for e, space in enumerate(["h1", "h10", "h1gauss"]):
+    data_path = data_directory / f"integrals_{space}_embedding_sampling.npy"
+    print(f"Loading sample statistics from '{data_path}'")
+    integrals = np.load(data_path)
+    dimension = len(integrals)
+    print(f"Plotting to axes {e}")
+    violin = ax[e].violinplot(list(integrals), positions=np.arange(1, len(integrals)+1))
+    violin['cbars'].set_linewidth(1)
+    violin['cmins'].set_linewidth(1)
+    violin['cmaxes'].set_linewidth(1)
+    ax[e].set_xlabel("$i$")
+    ax[e].set_xticks(np.arange(1, dimension+1, 1))
+    ax[e].set_yticks(np.arange(1, dimension+1, 1))
+    ax[e].set_title(titles[e])
+    ax[e].yaxis.grid()
+ax[0].set_ylabel("$Z_i$", rotation=0)
 
 fig.tight_layout()
 
-plot_directory = Path(__file__).parent / "plot"
-plot_directory.mkdir(exist_ok=True)
 plot_path = plot_directory / "Z_integral_plot.png"
 print(f"Saving Z integral statistics plot to '{plot_path}'")
 plt.savefig(plot_path, dpi=300, edgecolor="none", bbox_inches="tight", transparent=True)
